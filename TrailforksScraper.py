6e8a255a5698fbf3648af14321a6c068ed5d56bb
Iqra Imtiaz <iqraimtiaz@Iqras-Air.lan>
2022-11-11 23:56:26 -0500

    adding checkins per year

--- TrailforksScraper.py
+++ TrailforksScraper.py
@@ -3,6 +3,7 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from io import StringIO
 
 
 class trailforksScrapper:
@@ -79,6 +80,37 @@ class trailforksScrapper:
                 soup = BeautifulSoup(page.content, 'html.parser')
                 table = soup.find_all('table')
                 self.data = pd.concat([self.data,pd.read_html(str(table))[0]])
+        except Exception as e:
+            print('Error occurred')
+            print(e)
+
+        return self.data
+    
+    def fetchTrailStats(self,trail):
+        '''
+        Scrapes trails data from trailforks
+
+        Inputs:
+            trail: name of trail such as rattlesnake-ledge-trail
+        Returns:
+            Dataframe containing trail stats 
+        '''
+
+        # Specify headers, url and params
+        try:
+            self.data = pd.DataFrame()
+            search_url = self.url + '/trails/'+ trail+ '/stats/'
+            # Request the page and use BeautifulSoup to extract the contents
+            page = requests.get(search_url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            checkins_per_year = str(soup.find('div',class_='col-6 last'))
+            checkins_per_year = checkins_per_year[checkins_per_year.index('Year')-2:checkins_per_year.rindex(']')+1]
+
+            df = pd.read_csv(StringIO(checkins_per_year))
+            checkins_per_year = df.iloc[:,:2]
+            #checkins_per_year.replace('[','',regex=True)
+            self.data = checkins_per_year
+                
         except Exception as e:
             print('Error occurred')
             print(e)
