63a51dc9994e6db657246abc1030064effde3dcd
Andrew Bonafede <abonafede5@gmail.com>
2022-11-20 12:35:01 -0500

    Added initial demo script and script version of model

--- demo.py
+++ demo.py
@@ -2,9 +2,19 @@ import pandas as pd
 import numpy as np 
 import streamlit as st 
 from datetime import date, time
+from recommendation import get_trails
 
-zipcode = st.text_input('Please input your your zipcode.')
-radius = st.number_input('Please input how far you are willing to travel (in miles).')
-hour_of_day = st.time_input('What time would you like to go for a hike?', time(8, 45))
-date = st.date_input("What day would you like to go for a hike?", date.today())
+placeholder = st.empty()
+
+with placeholder.container():
+    zipcode = st.text_input('Please input your your zipcode.')
+    radius = st.number_input('Please input how far you are willing to travel (in miles).')
+    hour_of_day = st.time_input('What time would you like to go for a hike?', time(5, 45)).hour
+    date = st.date_input("What day would you like to go for a hike?", date.today())
+    btn = st.button('Find Recommended Trails')
+if btn:
+    placeholder.empty()
+    trails = get_trails(zipcode,radius,hour_of_day)
+    st.write(trails)
+    st.map(trails)
 
